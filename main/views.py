1a418d8824db42d33a5bda899c1f55c3daaa7dc7
HXY <762330305@qq.com>
2020-09-09 23:33:15 +0800

    achieve demo

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import cv2
 import numpy as np
+from .evaluate import predict
 
 
 def index(request):
@@ -14,5 +14,7 @@ def readImage(request):
     content = f.read()
     img = cv2.imdecode(np.frombuffer(content, np.uint8), cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    # cv2.imwrite('./photo.jpg', img)
-    return HttpResponse('teddddddddddd')
+    cv2.imwrite('./main/static/test/eval.jpg', img)
+    ans = predict('./main/static/test/eval.jpg')
+    ans = str(ans[0]) + str(ans[1]) + '.' + str(ans[2]) + str(ans[3])
+    return render(request, 'index.html', {'havephoto': True, 'ans': ans})
